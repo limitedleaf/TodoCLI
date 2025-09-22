c59ba1bbe884cecfd78d7a2eaef94186b5f4bcf2
Limited Leaf <limitedleaf7@gmail.com>
2025-09-23 01:14:28 +0530

    added vector2d added render_obj added renderer

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
+from src.core import vector2d
+from src.interface import render_item
+from src.interface import renderer
+
+
 def main():
-    print("Hello World!")
+    
+    test_obj = render_item.new()
+    render_item.set_name(test_obj, "Test Object")
+    render_item.set_mesh(test_obj, "x" * 25)
+    render_item.set_position(test_obj, vector2d.new(0.5, 0.5))
+    render_item.set_size(test_obj, vector2d.new(5, 5))
+    render_item.set_centralized(test_obj, True)
+
+    renderer.add(test_obj)
+    
+    while True:
+        renderer.render()
